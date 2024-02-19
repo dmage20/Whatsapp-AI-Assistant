87ad2d1f6e2940525abe577f1ffcb226166a3225
dmage20 <dmage20@gmai.com>
2024-02-18 18:09:05 -0800

    Heroku setup

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,7 +1,7 @@
 # config/initializers/twilio.rb
 
-Twilio.configure do |config|
-  config.account_sid = ENV["ACCOUNT_SID"]
-  config.auth_token = ENV["AUTH_TOKEN"]
-end
+# Twilio.configure do |config|
+#   config.account_sid = ENV["ACCOUNT_SID"]
+#   config.auth_token = ENV["AUTH_TOKEN"]
+# end
   
